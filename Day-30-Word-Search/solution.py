da9ba79d55b474c833abb6992bd187f68509aecd
Prince Sherathiya <126652751+Aicodebyprince@users.noreply.github.com>
2026-01-31 17:36:16 +0530

    Update solution.py

--- Day-30-Word-Search/solution.py
+++ Day-30-Word-Search/solution.py
@@ -1,1 +1,39 @@
+class Solution(object):
+    def exist(self, board, word):
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c, index):
+            # All characters matched
+            if index == len(word):
+                return True
+
+            # Out of bounds or mismatch
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return False
+            if board[r][c] != word[index]:
+                return False
+
+            # Mark cell as visited
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            # Explore neighbors
+            found = (
+                dfs(r + 1, c, index + 1) or
+                dfs(r - 1, c, index + 1) or
+                dfs(r, c + 1, index + 1) or
+                dfs(r, c - 1, index + 1)
+            )
+
+            # Restore cell (backtrack)
+            board[r][c] = temp
+            return found
+
+        # Try starting from every cell
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i, j, 0):
+                    return True
+
+        return False
 
